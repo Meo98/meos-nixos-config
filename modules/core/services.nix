a02037f2f6efbb9352e16b692fe57370fb5e7816
Meo98 <chenevard.romeo@gmail.com>
2026-05-13 22:02:27 +0200

    chore: auto-sync from meo 2026-05-13 22:02

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -56,7 +56,7 @@
                   {
                     type = "ladspa";
                     name = "rnnoise";
-                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+                    plugin = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa";
                     label = "noise_suppressor_mono";
                     control = {
                       "VAD Threshold (%)" = 50.0;
